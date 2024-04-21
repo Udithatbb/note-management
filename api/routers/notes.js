83280d640f161916b82854d6b42f012398dc00aa
Udith dev <udith@badbyte.cc>
2024-04-21 23:53:58 +0530

    create crud functions

--- api/routers/notes.js
+++ api/routers/notes.js
@@ -44,24 +44,56 @@ router.get("/notes/get", (req, res) => {
 
 //update Notes
 
-// router.put("/notes/update/:id", (req, res) => {
-//   // Notes.findByIdAndUpdate(
-//   //   req.params.id,
-//   //   {
-//   //     $set: req.body,
-//   //   },
-//   //   (err, notes) => {
-//   //     if (err) {
-//   //       return res.status(400).json({ error: err });
-//   //     }
+router.put("/notes/update/:id", (req, res) => {
+  Notes.findByIdAndUpdate(
+    req.params.id,{
+      $set:req.body,
+    },
+    {new:true, runValidators:true}  // Return the updated document and run validators
+  )
+  .then((updatedNote)=>{
+    if(!updatedNote){
+      return res.status(404).json({
+        error:'Note not found'
+      });
+    }
 
-//   //     return res.status(200).json({
-//   //       success: "Updated Succesfully",
-//   //     });
-//   //   }
-//   // );
-// });
+    return res.status(200).json({
+      success:"Update Succesfully",
+      data:updatedNote,//Return the update note data
+    });
+  })
+
+  .catch((err)=>{
+    console.error(err);
+    return res.status(400).json({error:err.message || 'An error occurred during the update'});
+  });
+});
+
+//  Delete Post
 
 
+
+router.delete('/notes/delete/:id', (req, res) => {
+  Notes.findByIdAndDelete(req.params.id)
+    .then((deletedNote) => {
+      if (!deletedNote) {
+        return res.status(404).json({
+          message: 'Note not found',
+        });
+      }
+
+      return res.json({
+        message: 'Delete Successful',
+        deletedNote,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        message: 'Delete Unsuccessful',
+        error: err.message || err,
+      });
+    });
+});
 
 module.exports = router;
